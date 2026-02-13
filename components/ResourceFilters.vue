5c24cc89ec550f9258c7ac85fc43da32ec7a3ae3
Fixer <fixer@bank-sampah.sulhi.workers.dev>
2026-02-13 08:36:39 +0000

    style: fix lint warnings in Vue components

--- components/ResourceFilters.vue
+++ components/ResourceFilters.vue
@@ -118,7 +118,10 @@
         :aria-label="contentConfig.filters.ariaLabels.dateAdded"
         class="space-y-2"
       >
-        <label class="flex items-center" :for="'date-anytime'">
+        <label
+          class="flex items-center"
+          :for="'date-anytime'"
+        >
           <input
             id="date-anytime"
             type="radio"
@@ -127,12 +130,15 @@
             :checked="selectedDateRange === 'anytime'"
             class="h-4 w-4 text-gray-600 border-gray-300 focus:ring-gray-500"
             @change="onDateRangeChange('anytime')"
-          />
+          >
           <span class="ml-2 text-sm text-gray-800">{{
             contentConfig.filters.dateRanges.any
           }}</span>
         </label>
-        <label class="flex items-center" :for="'date-last-week'">
+        <label
+          class="flex items-center"
+          :for="'date-last-week'"
+        >
           <input
             id="date-last-week"
             type="radio"
@@ -141,12 +147,15 @@
             :checked="selectedDateRange === 'lastWeek'"
             class="h-4 w-4 text-gray-600 border-gray-300 focus:ring-gray-500"
             @change="onDateRangeChange('lastWeek')"
-          />
+          >
           <span class="ml-2 text-sm text-gray-800">{{
             contentConfig.filters.dateRanges.week
           }}</span>
         </label>
-        <label class="flex items-center" :for="'date-last-month'">
+        <label
+          class="flex items-center"
+          :for="'date-last-month'"
+        >
           <input
             id="date-last-month"
             type="radio"
@@ -155,12 +164,15 @@
             :checked="selectedDateRange === 'lastMonth'"
             class="h-4 w-4 text-gray-600 border-gray-300 focus:ring-gray-500"
             @change="onDateRangeChange('lastMonth')"
-          />
+          >
           <span class="ml-2 text-sm text-gray-800">{{
             contentConfig.filters.dateRanges.month
           }}</span>
         </label>
-        <label class="flex items-center" :for="'date-last-year'">
+        <label
+          class="flex items-center"
+          :for="'date-last-year'"
+        >
           <input
             id="date-last-year"
             type="radio"
@@ -169,7 +181,7 @@
             :checked="selectedDateRange === 'lastYear'"
             class="h-4 w-4 text-gray-600 border-gray-300 focus:ring-gray-500"
             @change="onDateRangeChange('lastYear')"
-          />
+          >
           <span class="ml-2 text-sm text-gray-800">{{
             contentConfig.filters.dateRanges.year
           }}</span>
