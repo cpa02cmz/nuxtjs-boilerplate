56fb68ae96d81d0c871e813fc8db3a8491174b3a
cpa02cmz <226034710+cpa02cmz@users.noreply.github.com>
2026-02-16 06:03:10 +0000

    style: Fix lint warnings across components
    - Fix vue/max-attributes-per-line warnings
    - Fix vue/html-self-closing warnings
    - Fix vue/html-indent warnings
    - Fix vue/html-closing-bracket-newline warnings

--- components/ShareButton.vue
+++ components/ShareButton.vue
@@ -68,9 +68,9 @@
         copySuccess
           ? contentConfig.share.ariaLabels.copySuccess
           : contentConfig.share.ariaLabels.shareTitle.replace(
-              '{{title}}',
-              title
-            )
+            '{{title}}',
+            title
+          )
       "
       :aria-expanded="showShareMenu"
       :class="[
@@ -133,7 +133,10 @@
         aria-labelledby="share-menu"
         @keydown="handleMenuKeydown"
       >
-        <div class="py-1" role="none">
+        <div
+          class="py-1"
+          role="none"
+        >
           <!-- Twitter -->
           <a
             :href="twitterUrl"
@@ -259,7 +262,10 @@
               stroke-linecap="round"
               stroke-linejoin="round"
             >
-              <path class="checkmark-path" d="M4 10l4 4 8-8" />
+              <path
+                class="checkmark-path"
+                d="M4 10l4 4 8-8"
+              />
             </svg>
             {{
               copySuccess
