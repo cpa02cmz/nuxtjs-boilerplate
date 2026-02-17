f988c6d4c3580a46ec7870f862809e0fcd7eb73b
cpa02cmz <226034710+cpa02cmz@users.noreply.github.com>
2026-02-17 21:24:48 +0000

    style: Fix lint warnings

--- components/FilterSection.vue
+++ components/FilterSection.vue
@@ -1,5 +1,8 @@
 <template>
-  <fieldset class="mb-6" @keydown="handleKeydown">
+  <fieldset
+    class="mb-6"
+    @keydown="handleKeydown"
+  >
     <div class="flex items-center justify-between mb-3">
       <button
         v-if="collapsible"
@@ -120,7 +123,7 @@
               <span
                 v-if="
                   !prefersReducedMotion &&
-                  (recentlySelected === option || recentlyDeselected === option)
+                    (recentlySelected === option || recentlyDeselected === option)
                 "
                 class="checkbox-bloom absolute inset-0 rounded pointer-events-none"
                 :class="{
@@ -149,7 +152,7 @@
                 }"
                 @change="toggleOption(option)"
                 @click.stop
-              />
+              >
             </div>
             <label
               :for="`${id}-${option}`"
